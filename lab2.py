53adee25ef51839dfcf26aa9387514a6fb76abd6
EC2 Default User <ec2-user@ip-172-31-100-65.ec2.internal>
2020-05-19 15:58:29 +0000

    added my lab2.py file

---
+++ lab2.py
@@ -0,0 +1,40 @@
+"""
+Lab 2
+"""
+
+# 4.1
+my_name = "Tom"
+print(my_name.upper())
+
+# 4.2 
+my_id = 123
+print(my_id)
+
+#4.3
+# we cannot do this 123 = my_id
+my_id=your_id=123
+print(my_id) 
+print(your_id)
+
+#4.4
+my_id_str = "123"
+print(my_id_str)
+
+#4.5
+# this does not work print(my_name + my_id)
+
+#4.6
+print(my_name + my_id_str)
+
+#4.7
+print(my_name * 3)
+
+#4.8
+print("hello, world. This is my first python string.".split('.'))
+
+#4.9
+# must use double quotation mark if you are using a single quotation mark in the sentence message = 'Tom's id is 123'
+message = "Tom's ID is 123"
+print(message)
+
+
