abca162d082aaaabee48979aef464ae99f6e4875
EC2 Default User <ec2-user@ip-172-31-100-65.ec2.internal>
2020-05-20 16:03:12 +0000

    added my lab3.py file

---
+++ lab3.py
@@ -0,0 +1,47 @@
+"""
+Your module description
+"""
+
+#3.1
+str_list=['a','d','e','b','c']
+print(str_list)
+str_list.sort()
+print(str_list)
+
+#3.2
+str_list.append('f') #must add the quotations on the f or else it will be an error
+print(str_list)
+
+#3.3
+str_list.remove('d') #must add the quotations on the d or else it will be an error
+print(str_list)
+
+#3.4
+print(str_list[2])
+
+#3.5
+my_list = ['a','123',123,'b','B','False',False,123,None,'None']
+print(my_list)
+print(len(set(my_list)))
+
+#3.6
+print(len('This is my third python lab.'.split())) #asking for words, not letters, so we use "split" to have the "len" count the words
+
+#3.7
+num_list = [12,32,43,35]
+num_list.sort()
+print(num_list[0]) #min
+print(num_list[-1]) #max
+
+#3.8
+game_board = [[0,0,0],
+              [0,0,0],
+              [0,0,0]]
+print(game_board)
+game_board[1][1]=1
+print(game_board)
+
+
+
+
+
---
+++ lec3.py
@@ -0,0 +1,64 @@
+"""
+lecture notes for day 3 week 1
+"""
+
+#my_list = [1,2,3,4,5]
+#print(my_list)
+#my_list[0]=6
+#print(my_list)
+
+#print(my_list + [8,9])
+
+#my_list.extend(['a','b'])
+#print(my_list)
+#print('a' in my_list)
+#print(len(my_list))
+#print(0 in my_list)
+
+#my_list.sort()
+#print(my_list)
+
+#my_list.reverse()
+#print(my_list)
+
+#my_list.remove(7)
+#print(my_list)
+#print(my_list[-1])
+#print(my_list[0:2])
+#print(my_list[:3])
+#print(my_list[3:])
+
+
+#my_list [0]=6
+#print(my_list)
+
+#my_nested_list = [1,2,3,my_list]
+#print(my_nested_list)
+
+#my_letters=['a','b','c']
+#print(my_letters)
+#print(my_letters[0])
+#print(my_letters[0:3])
+#print(my_letters[:])
+
+#x,y,z = ['a','b','c']
+#print(x)
+#print(y)
+#print(z)
+
+#print('helloworld')
+#print('hello\tworld')
+#print('hello\nworld')
+#print(len('helloworld'))
+#print(len('hello world'))
+#print('helloworld'[0])
+
+my_letters = ['a','a','b','b','c']
+print(my_letters)
+#print(set(my_letters))
+
+my_unique_letters = set(my_letters)
+#print(my_unique_letters)
+#print(len(my_unique_letters))
+#print('a' in my_unique_letters)
+#print(list(my_unique_letters)[0])
