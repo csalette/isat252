e706800c49391d838d06735e398c17ccc20480f0
EC2 Default User <ec2-user@ip-172-31-100-65.ec2.internal>
2020-05-21 15:52:56 +0000

    Day 4 today

---
+++ lec4.py
@@ -0,0 +1,35 @@
+"""
+Lecture 4 
+"""
+#my_tuple = 'a','b','c','d','e'
+#print(my_tuple)
+#print(my_tuple[1]) # using index numbers to find the letters we want 
+#print(my_tuple[0:3]) # using slicing to find multiple letters at once
+#print(my_tuple[:])
+
+#my_2nd_tuple = ('a','b','c','d','e') #as long as you have he commas, it is a tuple.
+#print(my_2nd_tuple)
+
+#test = ('a',)
+
+#print(type(test,))
+
+#my_car = {
+            #'color':'red',
+            #'maker':'toyota',
+            #'year':2015
+            #}
+#print(my_car)
+#print(my_car['year'])
+#print(my_car['color'])
+#print(my_car.items())
+#print(my_car.keys())
+#print(my_car.values())
+#print(my_car.get('year'))
+
+#my_car['model']='corolla'
+#print(my_car)
+#my_car['year']='2020'
+#print(my_car)
+#print(len(my_car))
+#print('color' in my_car)
