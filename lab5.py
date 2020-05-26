6e98865f5bded9673d309dd49b7d9008d629450b
EC2 Default User <ec2-user@ip-172-31-100-65.ec2.internal>
2020-05-26 15:48:13 +0000

    Day 5 of Python

---
+++ lab5.py
@@ -0,0 +1,45 @@
+"""
+Lab 5
+"""
+#3.1
+alien_color = 'blue'
+if alien_color == 'blue':
+    print('you got 5 points!')
+
+#3.2
+alien_color = 'blue'
+if alien_color == 'blue':
+    print('you got 5 points!')
+else:
+        print('you got 10 points')
+        
+#3.3
+favorite_fruits =['apple','watermelon''banana']
+if 'watermelon' in favorite_fruits:
+    print('you really like watermelon')
+    
+if 'apple' in favorite_fruits: 
+    print('you really like apple')
+    
+if 'grape' in favorite_fruits:
+    print('you really like grape')
+    
+if 'banana' in favorite_fruits:
+    print('you really like banana')
+    
+if 'cherry' in favorite_fruits:
+    print('you really like cherry')
+    
+#3.4
+age = 25
+if age < 10:
+    print('the person is a kid')
+elif age < 20:
+    print('the person is a teenager')
+else:
+    print('the person is an adult')
+if age > 65:
+    print('the person is elder')
+
+
+        
---
+++ lec5.py
@@ -0,0 +1,43 @@
+"""
+lecture 5
+"""
+#a = [1,2,3]
+#b = [1,2,3]
+
+#print(a == b)
+#print(a is b)
+
+#print(id(a))
+#print(id(b))
+
+#x = None
+#print(id(None))
+#print(id(x))
+#print(x is None)
+#print(x == None)
+
+#y = []
+#x = None
+
+#print(type(y))
+#print(y == None)
+#print(y is None)
+#print(x is None)
+
+#print(True and False )
+#print (True or False)
+#print(not True)
+#print(not None)
+#print(not '0') #when paranthesis are used, it is a string
+#print(not 0) #when there are no parenthesis used, it is an integer
+#print(() and [])
+
+#if 2>1 :
+  #  if 1.5>1:
+   #     print('1.5>1')
+   #     print('2>1')
+   
+#if 2<=1:
+   # print('2<=1')
+#else: 
+   # print('2>1')
